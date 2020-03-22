bca8af0842718c0f70bfd71f8e8f8328c617b5f3
thangjik12 <kayami98@gmail.com>
2020-03-22 17:14:50 +0700

    showing vs-code github setup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import uuid from 'uuid';
 import TodoList from './components/TodoList';
 import TodoInput from './components/TodoInput';
-
+// showing vs-code github setup
 
 class App extends Component {
   render() {
